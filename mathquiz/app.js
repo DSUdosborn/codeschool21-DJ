9cbdd7d4e8882e21b7a3e6fe6d3ea784e4234560
DJ . Holt <djholt@CIT-SCCT12-1206.cs.dixie.edu>
2021-06-07 12:11:05 -0600

    Add math quiz

---
+++ mathquiz/app.js
@@ -0,0 +1,29 @@
+function newRandomProblem() {
+  var r1 = Math.floor(Math.random() * 100);
+  var r2 = Math.floor(Math.random() * 100);
+  var questionLeftSpan = document.querySelector("#question-left");
+  var questionRightSpan = document.querySelector("#question-right");
+  questionLeftSpan.innerHTML = r1;
+  questionRightSpan.innerHTML = r2;
+  var correctAnswer = r1 + r2;
+  return correctAnswer;
+}
+
+var correctAnswer = newRandomProblem();
+
+var checkAnswerButton = document.querySelector("#submit-answer");
+checkAnswerButton.onclick = function () {
+  var answerInput = document.querySelector("#answer-field");
+  var feedbackSpan = document.querySelector("#feedback");
+
+  if (answerInput.value == correctAnswer) {
+    feedbackSpan.innerHTML = "Correct!";
+    feedbackSpan.style.color = "#00FF00";
+    answerInput.value = "";
+    correctAnswer = newRandomProblem();
+  } else {
+    feedbackSpan.innerHTML = "Incorrect!";
+    feedbackSpan.style.color = "#FF0000";
+  }
+};
+
